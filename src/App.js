6da65a7142e7fd33abe9c2ea06744cf60c1266be
Full Name <perdanarobert9@gmail.com>
2024-12-04 09:50:30 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+//oke
 import AuthService from "./services/auth.service";
 //test
 import Login from "./components/Login";
